25cc837d64a88263885e941856258d00cc034503
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2012-03-03 15:03:42 +0000

    added js ui ot the system and started the popup for removing users

--- jiggly/public/assets/js/script.js
+++ jiggly/public/assets/js/script.js
@@ -9,6 +9,34 @@ $(document).ready(function () {
         setInterval(fadeFlashMsg,2000);
         
     }
+    
+    
+    // Js for dialog box's on manage users page
+    if ($('.dialoge_remove_user').length > 0){
+        
+        
+        
+        $('.dialoge_remove_user').dialog({
+                autoOpen: false,
+                width: 600,
+                buttons: {
+                        "Ok": function() { 
+                            $(this).dialog("close");
+                        }, 
+                        "Cancel": function() { 
+                            $(this).dialog("close"); 
+                        } 
+                }
+        });
+        
+        $('.dialoge_remove_user_link').click(function(){
+                var currentDialog = $(this).attr('id');
+                $('#dialog_box_user_'+currentDialog).dialog('open');
+                return false;
+        });
+
+    }
+    
    
 })
 
