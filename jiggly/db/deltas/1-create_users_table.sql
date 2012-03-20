06c331079d4131791c19396ea7d46cabfc92beee
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2012-03-20 23:53:09 +0000

    removing old data files and changing build script to try and build database GF

---
+++ jiggly/db/deltas/1-create_users_table.sql
@@ -0,0 +1,22 @@
+--//
+
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) COLLATE utf8_bin NOT NULL,
+  `password` varchar(50) COLLATE utf8_bin DEFAULT NULL,
+  `first_name` varchar(150) COLLATE utf8_bin DEFAULT NULL,
+  `last_name` varchar(150) COLLATE utf8_bin NOT NULL,
+  `role` varchar(20) COLLATE utf8_bin NOT NULL,
+  `email_address` varchar(200) COLLATE utf8_bin NOT NULL,
+  `active` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  KEY `password` (`password`),
+  KEY `role` (`role`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3 ;
+
+--//@UNDO
+
+DROP TABLE `users`;
+
+--//
