5727c9eb27f1a27b29cf925305b3bbb504478b52
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2012-03-21 00:20:20 +0000

    settup sql for phing db updates

---
+++ jiggly/db/inital-setup/setup.sql
@@ -0,0 +1,11 @@
+/* This is needed for the phing command to run (db deploy) */
+
+CREATE TABLE changelog (
+  change_number BIGINT NOT NULL,
+  delta_set VARCHAR(10) NOT NULL,
+  start_dt TIMESTAMP NOT NULL,
+  complete_dt TIMESTAMP NULL,
+  applied_by VARCHAR(100) NOT NULL,
+  description VARCHAR(500) NOT NULL
+);
+mysql> ALTER TABLE changelog ADD CONSTRAINT Pkchangelog PRIMARY KEY (change_number, delta_set); 
