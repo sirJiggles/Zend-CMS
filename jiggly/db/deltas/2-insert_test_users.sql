16bc944a2466c58cd607f649bae22c61f9b1e52b
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2012-04-07 17:06:26 +0100

    adde drop table to the users table delta as query wont work if table exists

--- jiggly/db/deltas/2-insert_test_users.sql
+++ jiggly/db/deltas/2-insert_test_users.sql
@@ -1,5 +1,5 @@
-INSERT INTO `users` VALUES(1, 'gfuller', '36bdf929501b3f9dcc0428edbc5832c250e43152', 'Gareth', 'Fuller', 'admin', 'gareth-fuller@hotmail.co.uk', 1, '');
-INSERT INTO `users` VALUES(2, 'gfuller2', '36bdf929501b3f9dcc0428edbc5832c250e43152', 'Gareth', 'Editor', 'editor', 'gareth.fuller@studio24.net', 1, '');
+INSERT INTO `users` VALUES(1, 'gfuller', '36bdf929501b3f9dcc0428edbc5832c250e43152', 'Gareth', 'Fuller', 'admin', 'gareth-fuller@hotmail.co.uk', 1);
+INSERT INTO `users` VALUES(2, 'gfuller2', '36bdf929501b3f9dcc0428edbc5832c250e43152', 'Gareth', 'Editor', 'editor', 'gareth.fuller@studio24.net', 1);
 
 --//@UNDO
 
