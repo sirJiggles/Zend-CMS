83bd9641e7d64c1f4949645b653750486c683f5e
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2012-06-07 21:15:33 +0100

    testing drag events on ipad F

--- project/cms/public/assets/js/libs/nested-sortables.js
+++ project/cms/public/assets/js/libs/nested-sortables.js
@@ -11,8 +11,180 @@
  * http://www.opensource.org/licenses/mit-license.php
  */
 
-(function($) {
+function performMouseDrag(object, event){
+    //Compute the helpers position
+    object.position = object._generatePosition(event);
+    object.positionAbs = object._convertPositionTo("absolute");
 
+    if (!object.lastPositionAbs) {
+            object.lastPositionAbs = object.positionAbs;
+    }
+
+    //Do scrolling
+    if(object.options.scroll) {
+            var o = object.options, scrolled = false;
+            if(object.scrollParent[0] != document && object.scrollParent[0].tagName != 'HTML') {
+
+                    if((object.overflowOffset.top + object.scrollParent[0].offsetHeight) - event.pageY < o.scrollSensitivity)
+                            object.scrollParent[0].scrollTop = scrolled = object.scrollParent[0].scrollTop + o.scrollSpeed;
+                    else if(event.pageY - object.overflowOffset.top < o.scrollSensitivity)
+                            object.scrollParent[0].scrollTop = scrolled = object.scrollParent[0].scrollTop - o.scrollSpeed;
+
+                    if((object.overflowOffset.left + object.scrollParent[0].offsetWidth) - event.pageX < o.scrollSensitivity)
+                            object.scrollParent[0].scrollLeft = scrolled = object.scrollParent[0].scrollLeft + o.scrollSpeed;
+                    else if(event.pageX - object.overflowOffset.left < o.scrollSensitivity)
+                            object.scrollParent[0].scrollLeft = scrolled = object.scrollParent[0].scrollLeft - o.scrollSpeed;
+
+            } else {
+
+                    if(event.pageY - $(document).scrollTop() < o.scrollSensitivity)
+                            scrolled = $(document).scrollTop($(document).scrollTop() - o.scrollSpeed);
+                    else if($(window).height() - (event.pageY - $(document).scrollTop()) < o.scrollSensitivity)
+                            scrolled = $(document).scrollTop($(document).scrollTop() + o.scrollSpeed);
+
+                    if(event.pageX - $(document).scrollLeft() < o.scrollSensitivity)
+                            scrolled = $(document).scrollLeft($(document).scrollLeft() - o.scrollSpeed);
+                    else if($(window).width() - (event.pageX - $(document).scrollLeft()) < o.scrollSensitivity)
+                            scrolled = $(document).scrollLeft($(document).scrollLeft() + o.scrollSpeed);
+
+            }
+
+            if(scrolled !== false && $.ui.ddmanager && !o.dropBehaviour)
+                    $.ui.ddmanager.prepareOffsets(object, event);
+    }
+
+    //Regenerate the absolute position used for position checks
+    object.positionAbs = object._convertPositionTo("absolute");
+
+    //Set the helper position
+    if(!object.options.axis || object.options.axis != "y") object.helper[0].style.left = object.position.left+'px';
+    if(!object.options.axis || object.options.axis != "x") object.helper[0].style.top = object.position.top+'px';
+
+    //Rearrange
+    for (var i = object.items.length - 1; i >= 0; i--) {
+
+            //Cache variables and intersection, continue if no intersection
+            var item = object.items[i], itemElement = item.item[0], intersection = object._intersectsWithPointer(item);
+            if (!intersection) continue;
+
+            if(itemElement != object.currentItem[0] //cannot intersect with itself
+                    &&	object.placeholder[intersection == 1 ? "next" : "prev"]()[0] != itemElement //no useless actions that have been done before
+                    &&	!$.contains(object.placeholder[0], itemElement) //no action if the item moved is the parent of the item checked
+                    && (object.options.type == 'semi-dynamic' ? !$.contains(object.element[0], itemElement) : true)
+                    //&& itemElement.parentNode == object.placeholder[0].parentNode // only rearrange items within the same container
+            ) {
+
+                    $(itemElement).mouseenter();
+
+                    object.direction = intersection == 1 ? "down" : "up";
+
+                    if (object.options.tolerance == "pointer" || object._intersectsWithSides(item)) {
+                            $(itemElement).mouseleave();
+                            object._rearrange(event, item);
+                    } else {
+                            break;
+                    }
+
+                    // Clear emtpy ul's/ol's
+                    object._clearEmpty(itemElement);
+
+                    object._trigger("change", event, object._uiHash());
+                    break;
+            }
+    }
+
+    var parentItem = (object.placeholder[0].parentNode.parentNode &&
+                                        $(object.placeholder[0].parentNode.parentNode).closest('.ui-sortable').length)
+                                    ? $(object.placeholder[0].parentNode.parentNode)
+                                    : null,
+        level = object._getLevel(object.placeholder),
+        childLevels = object._getChildLevels(object.helper),
+        previousItem = object.placeholder[0].previousSibling ? $(object.placeholder[0].previousSibling) : null;
+
+    if (previousItem != null) {
+            while (previousItem[0].nodeName.toLowerCase() != 'li' || previousItem[0] == object.currentItem[0]) {
+                    if (previousItem[0].previousSibling) {
+                            previousItem = $(previousItem[0].previousSibling);
+                    } else {
+                            previousItem = null;
+                            break;
+                    }
+            }
+    }
+
+    var newList = document.createElement(o.listType);
+
+    newList.setAttribute("class", 'subList'); //For Most Browsers
+    newList.setAttribute("className", 'subList'); //For IE; harmless to other browsers.
+
+    object.beyondMaxLevels = 0;
+
+    // If the item is moved to the left, send it to its parent level
+    if (parentItem != null &&
+                    (o.rtl && (object.positionAbs.left + object.helper.outerWidth() > parentItem.offset().left + parentItem.outerWidth()) ||
+                    !o.rtl && (object.positionAbs.left < parentItem.offset().left))) {
+            parentItem.after(object.placeholder[0]);
+            object._clearEmpty(parentItem[0]);
+            object._trigger("change", event, object._uiHash());
+    }
+    // If the item is below another one and is moved to the right, make it a children of it
+    else if (previousItem != null &&
+                            (o.rtl && (object.positionAbs.left + object.helper.outerWidth() < previousItem.offset().left + previousItem.outerWidth() - o.tabSize) ||
+                            !o.rtl && (object.positionAbs.left > previousItem.offset().left + o.tabSize))) {
+            object._isAllowed(previousItem, level, level+childLevels+1);
+            if (!previousItem.children(o.listType).length) {
+                    previousItem[0].appendChild(newList);
+            }
+            previousItem.children(o.listType)[0].appendChild(object.placeholder[0]);
+            object._trigger("change", event, object._uiHash());
+    }
+    else {
+            object._isAllowed(parentItem, level, level+childLevels);
+    }
+
+    //Post events to containers
+    object._contactContainers(event);
+
+    //Interconnect with droppables
+    if($.ui.ddmanager) $.ui.ddmanager.drag(object, event);
+
+    //Call callbacks
+    object._trigger('sort', event, object._uiHash());
+
+    object.lastPositionAbs = object.positionAbs;
+}
+
+function performMouseStop(object, event, noPropagation){
+    // If the item is in a position not allowed, send it back
+    if (object.beyondMaxLevels) {
+
+            object.placeholder.removeClass(object.options.errorClass);
+
+            if (object.domPosition.prev) {
+                    $(object.domPosition.prev).after(object.placeholder);
+            } else {
+                    $(object.domPosition.parent).prepend(object.placeholder);
+            }
+
+            object._trigger("revert", event, object._uiHash());
+
+    }
+
+    // Clean last empty ul/ol
+    for (var i = object.items.length - 1; i >= 0; i--) {
+            var item = object.items[i].item[0];
+            object._clearEmpty(item);
+    }
+
+    $.ui.sortable.prototype._mouseStop.apply(object, arguments);
+
+}
+
+(function($) {
+        
+        var proto =  $.ui.mouse.prototype,
+        _mouseInit = proto._mouseInit;
+        
 	$.widget("mjs.nestedSortable", $.extend({}, $.ui.sortable.prototype, {
 
 		options: {
@@ -26,6 +198,8 @@
 			rtl: false,
 			isAllowed: function(item, parent) { return true; }
 		},
+                
+                
 
 		_create: function() {
 			this.element.data('sortable', this.element.data('nestedSortable'));
@@ -43,179 +217,61 @@
 			return $.ui.sortable.prototype.destroy.apply(this, arguments);
 		},
 
-		_mouseDrag: function(event) {
-
-			//Compute the helpers position
-			this.position = this._generatePosition(event);
-			this.positionAbs = this._convertPositionTo("absolute");
-
-			if (!this.lastPositionAbs) {
-				this.lastPositionAbs = this.positionAbs;
-			}
-
-			//Do scrolling
-			if(this.options.scroll) {
-				var o = this.options, scrolled = false;
-				if(this.scrollParent[0] != document && this.scrollParent[0].tagName != 'HTML') {
-
-					if((this.overflowOffset.top + this.scrollParent[0].offsetHeight) - event.pageY < o.scrollSensitivity)
-						this.scrollParent[0].scrollTop = scrolled = this.scrollParent[0].scrollTop + o.scrollSpeed;
-					else if(event.pageY - this.overflowOffset.top < o.scrollSensitivity)
-						this.scrollParent[0].scrollTop = scrolled = this.scrollParent[0].scrollTop - o.scrollSpeed;
-
-					if((this.overflowOffset.left + this.scrollParent[0].offsetWidth) - event.pageX < o.scrollSensitivity)
-						this.scrollParent[0].scrollLeft = scrolled = this.scrollParent[0].scrollLeft + o.scrollSpeed;
-					else if(event.pageX - this.overflowOffset.left < o.scrollSensitivity)
-						this.scrollParent[0].scrollLeft = scrolled = this.scrollParent[0].scrollLeft - o.scrollSpeed;
-
-				} else {
-
-					if(event.pageY - $(document).scrollTop() < o.scrollSensitivity)
-						scrolled = $(document).scrollTop($(document).scrollTop() - o.scrollSpeed);
-					else if($(window).height() - (event.pageY - $(document).scrollTop()) < o.scrollSensitivity)
-						scrolled = $(document).scrollTop($(document).scrollTop() + o.scrollSpeed);
-
-					if(event.pageX - $(document).scrollLeft() < o.scrollSensitivity)
-						scrolled = $(document).scrollLeft($(document).scrollLeft() - o.scrollSpeed);
-					else if($(window).width() - (event.pageX - $(document).scrollLeft()) < o.scrollSensitivity)
-						scrolled = $(document).scrollLeft($(document).scrollLeft() + o.scrollSpeed);
-
-				}
-
-				if(scrolled !== false && $.ui.ddmanager && !o.dropBehaviour)
-					$.ui.ddmanager.prepareOffsets(this, event);
-			}
-
-			//Regenerate the absolute position used for position checks
-			this.positionAbs = this._convertPositionTo("absolute");
-
-			//Set the helper position
-			if(!this.options.axis || this.options.axis != "y") this.helper[0].style.left = this.position.left+'px';
-			if(!this.options.axis || this.options.axis != "x") this.helper[0].style.top = this.position.top+'px';
-
-			//Rearrange
-			for (var i = this.items.length - 1; i >= 0; i--) {
-
-				//Cache variables and intersection, continue if no intersection
-				var item = this.items[i], itemElement = item.item[0], intersection = this._intersectsWithPointer(item);
-				if (!intersection) continue;
-
-				if(itemElement != this.currentItem[0] //cannot intersect with itself
-					&&	this.placeholder[intersection == 1 ? "next" : "prev"]()[0] != itemElement //no useless actions that have been done before
-					&&	!$.contains(this.placeholder[0], itemElement) //no action if the item moved is the parent of the item checked
-					&& (this.options.type == 'semi-dynamic' ? !$.contains(this.element[0], itemElement) : true)
-					//&& itemElement.parentNode == this.placeholder[0].parentNode // only rearrange items within the same container
-				) {
-
-					$(itemElement).mouseenter();
-
-					this.direction = intersection == 1 ? "down" : "up";
-
-					if (this.options.tolerance == "pointer" || this._intersectsWithSides(item)) {
-						$(itemElement).mouseleave();
-						this._rearrange(event, item);
-					} else {
-						break;
-					}
-
-					// Clear emtpy ul's/ol's
-					this._clearEmpty(itemElement);
-
-					this._trigger("change", event, this._uiHash());
-					break;
-				}
-			}
-
-			var parentItem = (this.placeholder[0].parentNode.parentNode &&
-							 $(this.placeholder[0].parentNode.parentNode).closest('.ui-sortable').length)
-				       			? $(this.placeholder[0].parentNode.parentNode)
-				       			: null,
-			    level = this._getLevel(this.placeholder),
-			    childLevels = this._getChildLevels(this.helper),
-			    previousItem = this.placeholder[0].previousSibling ? $(this.placeholder[0].previousSibling) : null;
-
-			if (previousItem != null) {
-				while (previousItem[0].nodeName.toLowerCase() != 'li' || previousItem[0] == this.currentItem[0]) {
-					if (previousItem[0].previousSibling) {
-						previousItem = $(previousItem[0].previousSibling);
-					} else {
-						previousItem = null;
-						break;
-					}
-				}
-			}
-
-			var newList = document.createElement(o.listType);
+		_mouseDrag: function(event){
                         
-                        newList.setAttribute("class", 'subList'); //For Most Browsers
-                        newList.setAttribute("className", 'subList'); //For IE; harmless to other browsers.
-
-			this.beyondMaxLevels = 0;
-
-			// If the item is moved to the left, send it to its parent level
-			if (parentItem != null &&
-					(o.rtl && (this.positionAbs.left + this.helper.outerWidth() > parentItem.offset().left + parentItem.outerWidth()) ||
-					!o.rtl && (this.positionAbs.left < parentItem.offset().left))) {
-				parentItem.after(this.placeholder[0]);
-				this._clearEmpty(parentItem[0]);
-				this._trigger("change", event, this._uiHash());
-			}
-			// If the item is below another one and is moved to the right, make it a children of it
-			else if (previousItem != null &&
-						(o.rtl && (this.positionAbs.left + this.helper.outerWidth() < previousItem.offset().left + previousItem.outerWidth() - o.tabSize) ||
-						!o.rtl && (this.positionAbs.left > previousItem.offset().left + o.tabSize))) {
-				this._isAllowed(previousItem, level, level+childLevels+1);
-				if (!previousItem.children(o.listType).length) {
-					previousItem[0].appendChild(newList);
-				}
-				previousItem.children(o.listType)[0].appendChild(this.placeholder[0]);
-				this._trigger("change", event, this._uiHash());
-			}
-			else {
-				this._isAllowed(parentItem, level, level+childLevels);
-			}
-
-			//Post events to containers
-			this._contactContainers(event);
-
-			//Interconnect with droppables
-			if($.ui.ddmanager) $.ui.ddmanager.drag(this, event);
-
-			//Call callbacks
-			this._trigger('sort', event, this._uiHash());
-
-			this.lastPositionAbs = this.positionAbs;
+                        performMouseDrag(this, event);
 			return false;
 
 		},
 
 		_mouseStop: function(event, noPropagation) {
-
-			// If the item is in a position not allowed, send it back
-			if (this.beyondMaxLevels) {
-
-				this.placeholder.removeClass(this.options.errorClass);
-
-				if (this.domPosition.prev) {
-					$(this.domPosition.prev).after(this.placeholder);
-				} else {
-					$(this.domPosition.parent).prepend(this.placeholder);
-				}
-
-				this._trigger("revert", event, this._uiHash());
-
-			}
-
-			// Clean last empty ul/ol
-			for (var i = this.items.length - 1; i >= 0; i--) {
-				var item = this.items[i].item[0];
-				this._clearEmpty(item);
-			}
-
-			$.ui.sortable.prototype._mouseStop.apply(this, arguments);
-
+                        
+                        performMouseStop(this, event, noPropagation);
+			
 		},
+                
+                _mouseInit: function() {
+                    this.element
+                    .bind( "touchstart." + this.widgetName, $.proxy( this, "_touchStart" ) );
+                    _mouseInit.apply( this, arguments );
+                },
 
+                _touchStart: function( event ) {
+                    if ( event.originalEvent.targetTouches.length != 1 ) {
+                        return false;
+                    }
+
+                    this.element
+                    .bind( "touchmove." + this.widgetName, $.proxy( this, "_touchMove" ) )
+                    .bind( "touchend." + this.widgetName, $.proxy( this, "_touchEnd" ) );
+
+                    this._modifyEvent( event );
+
+                    $( document ).trigger($.Event("mouseup")); //reset mouseHandled flag in ui.mouse
+                    this._mouseDown( event );
+
+                    return false;           
+                },
+
+                _touchMove: function( event ) {
+                    this._modifyEvent( event );
+                    this._mouseDrag( event );   
+                },
+
+                _touchEnd: function( event ) {
+                    this.element
+                    .unbind( "touchmove." + this.widgetName )
+                    .unbind( "touchend." + this.widgetName );
+                    this._mouseStop( event ); 
+                },
+
+                _modifyEvent: function( event ) {
+                    event.which = 1;
+                    var target = event.originalEvent.targetTouches[0];
+                    event.pageX = target.clientX;
+                    event.pageY = target.clientY;
+                },
+                
 		serialize: function(options) {
 
 			var o = $.extend({}, this.options, options),
