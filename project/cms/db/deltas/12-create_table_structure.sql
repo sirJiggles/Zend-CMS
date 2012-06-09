ccf70b1ac8d9dac5e5a2400bb9755de5125ffbfb
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2012-06-09 23:08:13 +0100

    adding new delta files

---
+++ project/cms/db/deltas/12-create_table_structure.sql
@@ -0,0 +1,10 @@
+CREATE TABLE  `structure` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    `structure` longtext NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3;
+
+--//@UNDO
+
+DROP TABLE `structure`;
+
+--//
---
+++ project/cms/db/deltas/13-insert_initial_structure.sql
@@ -0,0 +1,6 @@
+INSERT INTO `structure` VALUES(1, 'testdata');
+--//@UNDO
+
+DELETE FROM `structure` WHERE `id` = 1;
+
+--//
